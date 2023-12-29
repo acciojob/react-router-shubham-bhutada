2b2d31f1859d10d2ca8a3752652b9ee90ffe303b
Shubham <shubhambhutada7810@gmail.com>
2023-12-30 01:24:13 +0530

    testcase 1 commit

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+        <h1>About</h1>
+        <p>This is a Sample React Router program.</p>
+    </div>
+  )
+}
+
+export default About
--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,22 @@
 
 import React from "react";
 import './../styles/App.css';
+import { Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+      </ul>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/about" element={<About/>}></Route>
+      </Routes>
     </div>
   )
 }
